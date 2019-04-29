b9cf1486fe69ea5101efe5d42bf7084c47c58dfb
InsidiousKoalas <49494064+InsidiousKoalas@users.noreply.github.com>
2019-04-28 19:41:44 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,41 @@
+//******************************************************************************
+
+#include <msp430.h>
+
+int main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+
+  P3SEL |= BIT3+BIT4;                       // P3.3,4 = USCI_A0 TXD/RXD
+  UCA0CTL1 |= UCSWRST;                      // **Put state machine in reset**
+  UCA0CTL1 |= UCSSEL_2;                     // SMCLK
+  UCA0BR0 = 9;                              // 1MHz 115200 (see User's Guide)
+  UCA0BR1 = 0;                              // 1MHz 115200
+  UCA0MCTL |= UCBRS_1 + UCBRF_0;            // Modulation UCBRSx=1, UCBRFx=0
+  UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+  UCA0IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+
+  __bis_SR_register(GIE);       // Enter LPM0, interrupts enabled
+  while(1){
+	  UCA0TXBUF = '?';
+  }
+
+  __no_operation();                         // For debugger
+}
+
+// Echo back RXed character, confirm TX buffer is ready first
+#pragma vector=USCI_A0_VECTOR
+__interrupt void USCI_A0_ISR(void)
+{
+  switch(__even_in_range(UCA1IV,4))
+  {
+  case 0:break;                             // Vector 0 - no interrupt
+  case 2:                                   // Vector 2 - RXIFG
+    while (!(UCA0IFG&UCTXIFG));             // USCI_A0 TX buffer ready?
+//    UCA0TXBUF = UCA0RXBUF;                  // TX -> RXed character
+    break;
+  case 4:break;                             // Vector 4 - TXIFG
+  default: break;
+  }
+}
+
